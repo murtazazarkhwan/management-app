5f7c4c942153f0242ac2176b3cef14c0a4185227
Murtaza khan <murtazazarkhwan@gmail.com>
2020-05-18 15:47:22 +0500

    fixed an issue

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,7 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!, only: [:index]
   def index
-    @projects = current_user.team.projects
-    binding.pry
+    @projects = current_user&.team&.projects
   end
 
   def terms
